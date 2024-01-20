7e81d569211993d139052f920d4fd13408027c63
rweventos <rweventos.rj@gmail.com>
2024-01-20 06:59:55 -0300

    feat: added register schema for zod

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: 'Password required',
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email',
+  }),
+  password: z.string().min(6, {
+    message: 'Minimum 6 characters required',
+  }),
+  name: z.string().min(1, {
+    message: 'Name required',
+  }),
+});
